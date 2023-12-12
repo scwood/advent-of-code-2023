ed812cf653d66ffd5e547ce545dd46e46ec984ab
Spencer Wood <spencercwood@gmail.com>
2023-12-12 00:06:53 -0800

    Add day 1 solution

---
+++ day1/day1.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+
+function main() {
+  const buffer = fs.readFileSync(path.join(__dirname, "input.txt"));
+  const input = buffer.toString();
+  const lines = input.split(os.EOL);
+  const answer = lines.reduce((accumulator, current) => {
+    return accumulator + getCalibrationValue(current);
+  }, 0);
+  console.log(answer);
+}
+
+function getCalibrationValue(line) {
+  const digits = line.split("").filter((char) => /\d/.test(char));
+  const finalString = digits[0] + digits[digits.length - 1];
+  return parseInt(finalString);
+}
+
+if (require.main === module) {
+  main();
+}
